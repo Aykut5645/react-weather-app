649c9dbb763f61e60be6c9dc4b237a5e53354e5f
Aykut5645 <aykut.fatin@abv.bg>
2024-06-15 10:31:40 +0300

    Add initial container structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import './App.scss'
+import './App.scss';
 
 const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
-}
+    <div className="container">
+      <h1
+        style={{
+          color: 'white',
+          background: 'black',
+          padding: '1rem 0',
+          textAlign: 'center',
+          marginBottom: '3rem',
+        }}
+      >
+        Real Time Weather
+      </h1>
+      <div style={{ maxWidth: '75rem', margin: 'auto' }} className="content">
+        <div
+          style={{
+            color: 'white',
+            background: 'red',
+            marginBottom: '.8rem',
+            padding: '16rem',
+          }}
+        >
+          Current Weather
+        </div>
+        <div
+          style={{
+            display: 'grid',
+            gridGap: '.6rem',
+            gridTemplateColumns: '1fr 1fr',
+          }}
+        >
+          <div style={{ color: 'white', background: 'green', padding: '6rem' }}>
+            Next Day Weather
+          </div>
+          <div
+            style={{ color: 'white', background: 'orange', padding: '6rem' }}
+          >
+            Next Day Weather
+          </div>
+          <div
+            style={{ color: 'white', background: 'violet', padding: '6rem' }}
+          >
+            Next Day Weather
+          </div>
+          <div style={{ color: 'white', background: 'brown', padding: '6rem' }}>
+            Next Day Weather
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
