0050c871ef629860edcae1bd7a74a57ec04411a4
Aykut5645 <aykut.fatin@abv.bg>
2024-06-16 00:07:22 +0300

    Add base structure to current weather card

--- src/App.tsx
+++ src/App.tsx
@@ -39,12 +39,51 @@ const App = () => {
             borderRadius: 5,
           }}
         >
-          <Metric />
-          <img
-            alt="weather icon"
-            className="weather-icon"
-            src={`src/assets/icons/${data.weather[0]?.icon}.png`}
-          />
+          <div style={{}}>
+            <Metric />
+            <div style={{ display: 'flex', gap: '2rem', width: '100%' }}>
+              <div>
+                <img
+                  alt="weather icon"
+                  className="weather-icon"
+                  src={`src/assets/icons/${data.weather[0]?.icon}.png`}
+                />
+              </div>
+              <div style={{ flex: 1 }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    paddingBottom: '1.4rem',
+                    borderBottom: '1px solid white',
+                    marginBottom: '1.4rem',
+                  }}
+                >
+                  <div style={{ fontSize: '1.5rem' }}>
+                    <b>Location:</b> Sofia / Bulgaria
+                  </div>
+                  <div style={{ fontSize: '1.5rem' }}>
+                    <b>Today:</b> 5/12/2024
+                  </div>
+                </div>
+                <div style={{ fontSize: '1.5rem' }}>
+                  <b>Temperature:</b> 20&deg;
+                </div>
+                <div style={{ fontSize: '1.5rem' }}>
+                  <b>Weather condition:</b> Clouds (scattered clouds)
+                </div>
+                <div style={{ fontSize: '1.5rem' }}>
+                  <b>Feels like:</b> 18&deg;
+                </div>
+                <div style={{ fontSize: '1.5rem' }}>
+                  <b>Humidity:</b> 59%
+                </div>
+                <div style={{ fontSize: '1.5rem' }}>
+                  <b>Wind speed:</b> 8.75 meter/sec
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div
           style={{
@@ -54,7 +93,13 @@ const App = () => {
           }}
         >
           <div style={{ color: 'white', background: 'green', padding: '6rem' }}>
-            Next Day Weather
+            <div>
+              <img
+                alt="weather icon"
+                className="weather-icon"
+                src={`src/assets/icons/${data.weather[0]?.icon}.png`}
+              />
+            </div>
           </div>
           <div
             style={{ color: 'white', background: 'orange', padding: '6rem' }}
