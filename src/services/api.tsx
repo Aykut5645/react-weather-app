02796c1e5a1b53f5120ce6343d87f82d903319d1
Aykut5645 <aykut.fatin@abv.bg>
2024-06-18 10:26:01 +0300

    Add env variable

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -7,14 +7,14 @@ const fetchCurrentWeather = async (
   unit: UnitType
 ): Promise<CurrentWeatherResponseType> => {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=42.6926003&lon=23.3557927&appid=8c8665dfe9c59d77eb1e4d54ad9490a1&units=${unit}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=42.6926003&lon=23.3557927&appid=${import.meta.env.VITE_API_KEY}&units=${unit}`
   );
   return await res.json();
 };
 
 const fetchForecast = async (unit: UnitType) => {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=42.6926003&lon=23.3557927&appid=8c8665dfe9c59d77eb1e4d54ad9490a1&units=${unit}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=42.6926003&lon=23.3557927&appid=${import.meta.env.VITE_API_KEY}&units=${unit}`
   );
   const data: ForecastResponseType = await res.json();
   return getDailyMiddayWeather(data);
