2e18457f0990b6c9a2b7e4af19d192b3e6689316
Aykut5645 <aykut.fatin@abv.bg>
2024-06-24 13:35:53 +0300

    Change the logic for getting location

--- src/hooks/useCoordinates.tsx
+++ src/hooks/useCoordinates.tsx
@@ -1,52 +1,29 @@
 import { useState, useEffect } from 'react';
 import { CoordinatesType } from '../types/ForecastResponseType.tsx';
 
-import { PermissionStatus } from "../utils/enums.tsx";
-
 const useCoordinates = () => {
   const [coords, setCoords] = useState<CoordinatesType | null>(null);
-  const [errorCoords, setErrorCoords] = useState<{ message: string } | null>(
-    null
-  );
+  const [errorCoords, setErrorCoords] = useState<{ message: string } | null>(null);
+  const [permissionGranted, setPermissionGranted] = useState(false);
 
   useEffect(() => {
-    const checkPermissionAndFetchLocation = async () => {
-      try {
-        const permissionStatus = await navigator.permissions.query({ name: 'geolocation' });
-
-        if (permissionStatus.state === PermissionStatus.GRANTED) {
-          navigator.geolocation.getCurrentPosition(success, error);
-        } else if (permissionStatus.state === PermissionStatus.PROMPT) {
-          setErrorCoords({ message: 'Location access is needed to provide weather information.' });
-        } else if (permissionStatus.state === 'denied') {
-          setErrorCoords({ message: 'Location access has been denied. Please enable it in your browser settings.' });
-        }
-
-        permissionStatus.onchange = () => {
-          if (permissionStatus.state === PermissionStatus.GRANTED) {
-            navigator.geolocation.getCurrentPosition(success, error);
-          } else if (permissionStatus.state === PermissionStatus.DENIED) {
-            setErrorCoords({ message: 'Location access has been denied. Please enable it in your browser settings.' });
-          }
-        };
-      } catch (err) {
-        setErrorCoords({ message: 'An error occurred while checking location permissions.' });
-      }
-    };
-
     const success = (position: GeolocationPosition) => {
       setCoords({
         lat: position.coords.latitude,
         lon: position.coords.longitude,
       });
+      setPermissionGranted(true);
     };
 
     const error = (err: GeolocationPositionError) => {
       setErrorCoords({ message: err.message });
     };
 
-    checkPermissionAndFetchLocation();
-  }, []);
+
+    if (!permissionGranted) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }, [permissionGranted]);
 
   return { isFetchingCoords: !coords && !errorCoords, coords, errorCoords };
 };
