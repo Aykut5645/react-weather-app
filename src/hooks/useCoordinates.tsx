d5687b51280be5a6babddcd0a4a353bfcf8a5096
Aykut5645 <aykut.fatin@abv.bg>
2024-06-24 12:47:26 +0300

    Add improvement to useCoordinates.tsx

--- src/hooks/useCoordinates.tsx
+++ src/hooks/useCoordinates.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { CoordinatesType } from '../types/ForecastResponseType.tsx';
 
+import { PermissionStatus } from "../utils/enums.tsx";
+
 const useCoordinates = () => {
   const [coords, setCoords] = useState<CoordinatesType | null>(null);
   const [errorCoords, setErrorCoords] = useState<{ message: string } | null>(
@@ -8,6 +10,30 @@ const useCoordinates = () => {
   );
 
   useEffect(() => {
+    const checkPermissionAndFetchLocation = async () => {
+      try {
+        const permissionStatus = await navigator.permissions.query({ name: 'geolocation' });
+
+        if (permissionStatus.state === PermissionStatus.GRANTED) {
+          navigator.geolocation.getCurrentPosition(success, error);
+        } else if (permissionStatus.state === PermissionStatus.PROMPT) {
+          setErrorCoords({ message: 'Location access is needed to provide weather information.' });
+        } else if (permissionStatus.state === 'denied') {
+          setErrorCoords({ message: 'Location access has been denied. Please enable it in your browser settings.' });
+        }
+
+        permissionStatus.onchange = () => {
+          if (permissionStatus.state === PermissionStatus.GRANTED) {
+            navigator.geolocation.getCurrentPosition(success, error);
+          } else if (permissionStatus.state === PermissionStatus.DENIED) {
+            setErrorCoords({ message: 'Location access has been denied. Please enable it in your browser settings.' });
+          }
+        };
+      } catch (err) {
+        setErrorCoords({ message: 'An error occurred while checking location permissions.' });
+      }
+    };
+
     const success = (position: GeolocationPosition) => {
       setCoords({
         lat: position.coords.latitude,
@@ -19,7 +45,7 @@ const useCoordinates = () => {
       setErrorCoords({ message: err.message });
     };
 
-    navigator.geolocation.getCurrentPosition(success, error);
+    checkPermissionAndFetchLocation();
   }, []);
 
   return { isFetchingCoords: !coords && !errorCoords, coords, errorCoords };
--- src/utils/enums.tsx
+++ src/utils/enums.tsx
@@ -2,3 +2,9 @@ export enum TempScale {
   CELSIUS = 'celsius',
   FAHRENHEIT = 'fahrenheit',
 }
+
+export enum PermissionStatus {
+  GRANTED = 'granted',
+  DENIED = 'denied',
+  PROMPT = 'prompt',
+}
